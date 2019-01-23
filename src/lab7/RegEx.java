08a0e33660767805b977a1b8629b79394c3752d0
katie <kateann58@gmail.com>
2019-01-23 00:06:31 -0500

    cleaned code

--- src/lab7/RegEx.java
+++ src/lab7/RegEx.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class RegEx {
 static Scanner scnr = new Scanner(System.in);
+static int index;
 	
 	public static void main(String[] args) {
 		boolean retry = true;
@@ -11,34 +12,21 @@ static Scanner scnr = new Scanner(System.in);
 			String[] dataSet = { "Name", "email", "phone number", "date", "HTML elements" };
 			boolean valid;
 			
-			do {
-				valid = validateName(userInput(dataSet[0]));
-				validate(valid, dataSet[0]);
-			} while (!valid);
-			
-			do {
-				valid = validateEmail(userInput(dataSet[1]));
-				validate(valid, dataSet[1]);
-			} while (!valid);
-
-			do {
-				valid = validatePhone(userInput(dataSet[2]));
-				validate(valid, dataSet[2]);
-			} while (!valid);
-			
-			do {
-				valid = validateDate(userInput(dataSet[3]));
-				validate(valid, dataSet[3]);
-			} while (!valid);
-			
-			do {
-				valid = validateHtml(userInput(dataSet[4]));
-				validate(valid, dataSet[4]);
-			} while (!valid);
-			
+			for (String data : dataSet) {
+				for (int i = 0; i < dataSet.length; i++) {
+					if (dataSet[i] == data) {
+						index = i;
+					}
+				}
+				do {
+					valid = validateArray(userInput(data));
+					validate(valid, data);
+				} while (!valid);
+			}
 			retry = retry();
 		}
 		exit();
+		scnr.close();
 	}
 	
 	private static void greeting() {
@@ -52,6 +40,11 @@ static Scanner scnr = new Scanner(System.in);
 		return scnr.nextLine();
 	}
 	
+	private static boolean validateArray(String input) {
+		boolean[] methods = new boolean[] { validateName(input), validateEmail(input), validatePhone(input), validateDate(input), validateHtml(input) };
+		return methods[index];
+	}
+	
 	private static void validate(boolean valid, String data) {
 		if (valid) {
 			System.out.println(data + " is valid!");
@@ -60,23 +53,14 @@ static Scanner scnr = new Scanner(System.in);
 		}
 	}
 	
-//	private static void valid(String[] dataSet) {	
-//		String[] input = new String[5];
-//		for (int i = 0; i < 5; i++) {
-//			input[i] = userInput(dataSet[i]);
-//			String[] methods = { validateName(dataSet[i]), validateEmail(dataSet[i]), validatePhone(dataSet[i]), validateDate(dataSet[i]), validateHtml(dataSet[i]) };
-//		}
-//			boolean valid = validateName(userInput(data));
-//			if (valid) {
-//				System.out.println(data + " is valid!");
-//			} else {
-//				System.out.println("Sorry, " + data + " is not valid!");
-//			}	
-//		}
-//	}
-	
-	private static boolean validateName(String name) {
-		return name.matches("[A-Z]{1}[A-z]{0,30}");
+	private static boolean validateName(String fullName) {
+		String[] names = fullName.split(" ");
+		for (String name : names) {
+			if (!name.matches("[A-Z]{1}[A-z]{0,30}")) {
+				return false;
+			}
+		}
+		return true;
 		////can only have alphabetic chars, start with capital, max length 30
 	}
 	
@@ -92,29 +76,62 @@ static Scanner scnr = new Scanner(System.in);
 		////###-###-####
 	}
 	
-	private static boolean validateDate(String date) {
-		return date.matches("[0-3]{1}[0-9]{1}/[0-1]{1}[0-9]{1}/[1-2]{1}[09]{1}[0-9]{2}");
+	private static boolean validateDate(String fullDate) {
+		if (fullDate.matches("\\d{2}/\\d{2}/\\d{4}")) {
+			String[] dates = fullDate.split("/");
+			int month = Integer.parseInt(dates[1]);
+			if (month < 1 || month > 12) {
+				return false;
+			} 
+			int days = Integer.parseInt(dates[0]);
+			if (days < 1 || days > 31) {
+				return false;
+			}
+			int year = Integer.parseInt(dates[2]);
+			if (month == 2) {
+				if ((leapYear(year) && days > 29) || (!leapYear(year) && days > 28)) {
+					return false;
+				} 
+			}
+			if (days > maxDaysInMonth(month)) {
+				return false;
+			}
+		}
+		return true;
 		/////dd/mm/yyyy
+	}
+	
+	private static int maxDaysInMonth(int month) {
+		if (month == 2) {
+			return 29;
+		} else if (month == 9 || month == 4 || month == 6 || month == 11) {
+			return 30;
+		} else {
+			return 31;
+		}
+	}
+	
+	private static boolean leapYear(int year) {
+		if (year % 4 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	private static boolean validateHtml(String html) {
 		String[] splitStr = html.trim().split("\\s+");
 		if (!splitStr[0].matches("<[\\w$]+>")) {
-			for (String word : splitStr) {
-				System.out.print("( " + word + " )");
-			}
 			return false;
 		} else {
 			String element = "</" + splitStr[0].substring(1, splitStr[0].length());
-			if (element.matches(splitStr[1])) {
+			if (element.matches(splitStr[splitStr.length -1])) {
 				return true;
 			} else {
-				System.out.println("( " + element + " ) : ( " + splitStr[1] + " )");
 				return false;
 			}
 		}
-		//return html.matches("<[a-z]+[\\w$]>");
-		/////</p> jhdjfkhoadhf  </p> is valid
+		/////<p>  </p> is valid
 		/////<h1 </h1> is not valid
 	}
 	
